dbef48bb0945814b01aa8e44268a8c5fe6e30c29
Jordan Long <jordanleelong5@gmail.com>
2019-09-17 02:28:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 def introduction_with_language_optional(name, language)
-default value = ruby
+default_value language = ruby
 end 
 
 
