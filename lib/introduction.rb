dd5a6f62424e197236a4459d5794d6fa949945a9
Jordan Long <jordanleelong5@gmail.com>
2019-09-17 02:35:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,8 +5,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 def introduction_with_language_optional(name, language = "Ruby")
-expect {introduction_with_language_optional("Edwin")}.to output("Hi, my name is Edwin and I'm learning to program in Ruby.\n").to_stdout
-end
+
 
 
 
